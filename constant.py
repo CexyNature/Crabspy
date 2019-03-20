1f02e87c3d7d5508ac9f14d78cfd3b0101baee68
Cesar Herrera <29750401+CexyNature@users.noreply.github.com>
2019-03-20 11:01:59 +1000

    Constant module created

---
+++ constant.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+List of constants used in other modules.
+These should be define by the user to fit their experimental setting and purpose.
+
+DIM:
+    Store the quadrat's dimension (sides) in space units such as centimeters or meters.
+    All sides must be equal (i.e. square).
+
+COLOR_SET_0:
+    Define the color to use to draw reference elements in the image, such as:
+    digital quadrat, point vertices, mouse position, and organism position.
+
+CAPTURE_VERTICES: bool
+    If True will enable the capture of quadrat's vertices in the image using the mouse click function.
+    If False will import quadrat's vertices coordinates provided by user (i.e. QUADRAT_POINTS)
+
+QUADRAT_POINTS:
+    Known pixel coordinates of quadrat's vertices on image.
+"""
+
+DIM = [50, 50, 50, 50]
+COLOR_SET_0 = (243, 28, 20)
+CAPTURE_VERTICES = True
+QUADRAT_POINTS = [(628, 105), (946, 302), (264, 393), (559, 698)]
