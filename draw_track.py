bb278a2a6b74506c28f50a1b0aab7074377d9b96
Cesar Herrera <29750401+CexyNature@users.noreply.github.com>
2019-05-23 12:31:09 +1000

    Add alpha to track draw, and condition to handle NaN coordinates

--- draw_track.py
+++ draw_track.py
@@ -14,8 +14,9 @@ args = vars(ap.parse_args())
 
 track_meta = pd.read_csv("results/" + args["file"], header=0, nrows=1)
 track = pd.read_csv("results/" + args["file"], header=2, skiprows=range(0, 1))
-print(track.head())
-print(track.tail())
+# track = track.replace(np.nan, "", regex=True)
+# print(track.head())
+# print(track.tail())
 # print(track_meta.head())
 file_name = track_meta["file_name"].values[0]
 quadrat_vertices = [track_meta["vertice_1"].values[0],
@@ -38,7 +39,7 @@ M, side, vertices_draw, IM, conversion = methods.calc_proj(quadrat_vertices)
 
 pts = deque(maxlen=int(track_meta["length_video"].values[0])+250)
 (dX, dY) = (0, 0)
-target = 0
+target = 9000
 vid.set(1, target)
 counter = target
 # color1 = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
@@ -49,17 +50,24 @@ name_color, color1 = methods.select_color()
 while vid.isOpened():
     _, img = vid.read()
     result = cv2.warpPerspective(img, M, (side, side))
+    result2 = result.copy()
 
     if counter <= len(track.index)-1:
-        print(counter, track["Frame_number"].values[counter])
-        coord_x = track["Crab_position_x"].values[counter]
-        coord_y = track["Crab_position_y"].values[counter]
-        center = (coord_x, coord_y)
-        pts.appendleft(center)
 
-        for i in np.arange(1, len(pts)):
-            cv2.line(result, pts[i - 1], pts[i], color1, 1)
-            cv2.circle(result, (coord_x, coord_y), 15, color1, 1)
+        try:
+            # print(counter, track["Frame_number"].values[counter])
+            # coord_x = int(track["Crab_position_x"].iloc[[counter-15, counter+15]].mean())
+            # coord_y = int(track["Crab_position_y"].iloc[[counter-15, counter+15]].mean())
+            coord_x = int(track["Crab_position_x"].values[counter])
+            coord_y = int(track["Crab_position_y"].values[counter])
+            center = (coord_x, coord_y)
+            pts.appendleft(center)
+
+            for i in np.arange(1, len(pts)):
+                cv2.line(result, pts[i - 1], pts[i], color1, 1)
+                cv2.circle(result, (coord_x, coord_y), 15, color1, 1)
+        except (ValueError, IndexError):
+            pass
     else:
         pass
 
@@ -68,7 +76,10 @@ while vid.isOpened():
     cv2.putText(result, text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (10, 10, 10), 1)
     cv2.putText(result, "Frame n. {0:d}".format(counter), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (10, 10, 10), 1)
 
-    cv2.imshow("result", result)
+    result2 = cv2.addWeighted(result, 0.5, result2, 0.5, 0)
+
+
+    cv2.imshow("result", result2)
     counter += 1
 
     key = cv2.waitKey(1) & 0xFF
