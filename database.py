eac559c2c6db16d8ea84f53c49672f3193f79e94
Cesar Herrera <29750401+CexyNature@users.noreply.github.com>
2019-09-12 18:16:32 +1000

    Add functionality to handle a name not present in database

--- database.py
+++ database.py
@@ -4,6 +4,7 @@
 Consult and prune database
 """
 
+import argparse
 import pickle
 import methods
 
@@ -11,8 +12,12 @@ __author__ = "Cesar Herrera"
 __copyright__ = "Copyright (C) 2019 Cesar Herrera"
 __license__ = "GNU GPL"
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--database", default="GP010016", help="Provide path to video file")
+args = vars(ap.parse_args())
+
 prune = True
-video_name = "GP010016_fast"
+video_name = args["database"]
 file = "results/" + video_name
 
 if prune:
@@ -22,34 +27,33 @@ if prune:
     try:
         name = str(input("* Please enter name for entry you want to remove: "))
         individual_name = video_name + "_" + name
-        print("You are about to delete entry {} from database".format(individual_name))
-        answer = input("Confirm this action (type 'True') or cancel (type 'False'): ")
-        if answer == "True":
-            for i in database:
-                if i.crab_name == individual_name:
 
-                    res = [i for i in database if not (i.crab_name == individual_name)]
-                    print("This will be the result:\n")
-                    print(res)
-                    # del database[i]
-
-                    with open(file, "wb") as f:
-                        pickle.dump(res, f)
-                    break
-
-        elif answer == "False":
-            print("Operation halted, entry was not deleted")
-            pass
+        if next((item for item in database if item.crab_name == individual_name), False):
+            print("Name in database")
+            print("You are about to delete entry {} from database".format(individual_name))
+            answer = input("Confirm this action (type 'True') or cancel (type 'False'): ")
+            if answer == "True":
+                for i in database:
+                    if i.crab_name == individual_name:
+                        res = [i for i in database if not (i.crab_name == individual_name)]
+                        print("This will be the result:\n")
+                        print(res)
+                        # del database[i]
+                        with open(file, "wb") as f:
+                            pickle.dump(res, f)
+                        break
+            elif answer == "False":
+                print("Operation halted, entry was not deleted")
+                pass
+            else:
+                print("Unexpected answer. Operation halted")
+                pass
         else:
-            print("Unexpected answer. Operation halted")
-            pass
-
+            print("Name not found")
     except ValueError:
         print("Error in input. Database was not pruned")
-
     # print("Current database contains:\n")
     # print(database)
-
 else:
     database = methods.CrabNames.open_crab_names(file)
     print(database)
