5f83294fcc7c3fd7a5e6f1641ac742766c5ba5c8
Cesar Herrera <29750401+CexyNature@users.noreply.github.com>
2019-09-17 18:17:13 +1000

    Fix the issue that prevent the metadata associated to the track (i.e. header) to be saved after processing the dulicates

--- duplicates.py
+++ duplicates.py
@@ -8,6 +8,7 @@ This is normally product of tracking the same individual twice using the same or
 import os
 import argparse
 import pandas as pd
+import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
@@ -21,8 +22,15 @@ args = vars(ap.parse_args())
 
 # video_name = os.path.splitext(args["video"])[0].format()
 track = pd.read_csv("results/" + args["file"], header=2, skiprows=range(0, 1))
+track_meta = pd.read_csv("results/" + args["file"], header=None, nrows=2)
 duplicates = track[track.duplicated(["Frame_number"], keep=False)]
 
+# Find overlapping frames
+# Count how many sections overlap
+# Find the tracking method used for each section
+# Plot overlapping sections side by side
+# Request user to select one
+# Keep selection and delete overlap
 
 if (duplicates["tracker_method"] != "Manual_tracking").any():
     index_2_del = duplicates.index[duplicates.tracker_method != "Manual_tracking"].tolist()
@@ -52,17 +60,36 @@ if (duplicates["tracker_method"] != "Manual_tracking").any():
 else:
     print("You have duplicates, but none of these came from the method 'Manual_tracking'")
 
-print(duplicates)
-print(index_2_del)
-print(len(index_2_del))
-print(track.shape)
+# print(duplicates)
+# print(index_2_del)
+# print(len(index_2_del))
+# print(track.shape)
 track = track.drop(index_2_del)
-print(track.shape)
+# print(track.shape)
 
-# Find overlapping frames
-# Count how many sections overlap
-# Find the tracking method used for each section
-# Plot overlapping sections side by side
-# Request user to select one
-# Keep selection and delete overlap
+os.makedirs("results/processed_tracks/", exist_ok=True)
 
+new_file = "results/processed_tracks/Pro_" + args["file"]
+track.to_csv(new_file, index=False)
+
+# with open("results/" + args["file"], "r") as file:
+#     meta_information = [next(file) for x in range(3)]
+
+meta_information = track_meta.values.tolist()
+print(meta_information)
+print("######################## ##################### ################")
+empty = list()
+
+with open(new_file, "r") as result_in:
+    reader = list(csv.reader(result_in))
+    # print(reader[:10])
+    reader.insert(0, empty)
+    reader.insert(0, meta_information[1])
+    reader.insert(0, meta_information[0])
+    # print(reader[:10])
+
+
+with open(new_file, "w", newline="") as result_out:
+    writer = csv.writer(result_out)
+    for line in reader:
+        writer.writerow(line)
