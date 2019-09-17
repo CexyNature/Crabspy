ac0fd97193fa18b14d4093e008b00ffff059238c
Cesar Herrera <29750401+CexyNature@users.noreply.github.com>
2019-09-17 17:11:41 +1000

    Add functionality to plot duplicates, and keep manual_tracking over other methods

--- duplicates.py
+++ duplicates.py
@@ -9,6 +9,7 @@ import os
 import argparse
 import pandas as pd
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 __author__ = "Cesar Herrera"
 __copyright__ = "Copyright (C) 2019 Cesar Herrera"
@@ -18,8 +19,45 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", default="tracking_file.csv", help="Provide video name")
 args = vars(ap.parse_args())
 
-video_name = os.path.splitext(args["video"])[0].format()
+# video_name = os.path.splitext(args["video"])[0].format()
+track = pd.read_csv("results/" + args["file"], header=2, skiprows=range(0, 1))
+duplicates = track[track.duplicated(["Frame_number"], keep=False)]
 
+
+if (duplicates["tracker_method"] != "Manual_tracking").any():
+    index_2_del = duplicates.index[duplicates.tracker_method != "Manual_tracking"].tolist()
+    print("Priority is given to track positions (i.e. rows) that were tracked using the method 'Manual_tracking'")
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax2 = fig.add_subplot(1, 2, 2, sharex=ax1, sharey=ax1)
+
+    ax1.plot(duplicates.loc[duplicates.tracker_method == "Manual_tracking", "Crab_position_x"],
+             duplicates.loc[duplicates.tracker_method == "Manual_tracking", "Crab_position_y"])
+    ax1.title.set_text("Points got by manual tracking")
+    ax1.set_xlabel('Position X')
+    ax1.set_ylabel('Position Y')
+    ax1.set_aspect("equal", "box")
+
+    ax2.plot(duplicates.loc[duplicates.tracker_method != "Manual_tracking", "Crab_position_x"],
+             duplicates.loc[duplicates.tracker_method != "Manual_tracking", "Crab_position_y"])
+    ax2.title.set_text("Points got by MIL tracking (to be deleted)")
+    ax2.set_xlabel('Position X')
+    ax2.set_aspect("equal", "box")
+
+    plt.gca().invert_yaxis()
+    plt.show()
+
+    duplicates = duplicates[duplicates.tracker_method == "Manual_tracking"]
+else:
+    print("You have duplicates, but none of these came from the method 'Manual_tracking'")
+
+print(duplicates)
+print(index_2_del)
+print(len(index_2_del))
+print(track.shape)
+track = track.drop(index_2_del)
+print(track.shape)
 
 # Find overlapping frames
 # Count how many sections overlap
