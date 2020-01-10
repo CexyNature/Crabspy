67e22650a59a95393463d21b996fb9ead190eda5
Cesar Herrera <29750401+CexyNature@users.noreply.github.com>
2020-01-10 17:43:26 +1000

    Feat: Create Tool folder and add module to list video files and video files meta information

---
+++ crabspy/tools/README.md
@@ -0,0 +1,30 @@
+Crabspy Tools
+===================
+
+Useful tools for processing videos and images
+
+Requirements:
+
+- ffmpeg
+
+## Tools:
+```
+python list_videos.py
+```
+
+Module for recursively listing all videos in a folder and extracting meta information associated to videos
+
+The resulting file would look like:
+
+| file_name	| file_directory | file_extension | error | file_path | file_size | file_created | tag_major_brand | file_duration | duration_ts | nb_frames | fps | codec | codec_tag | aspect_ratio | width | height |
+| --------- | -------------- | -------------- | ----- | --------- | --------- | ------------ | --------------- | ------------- | ------------| --------- | --- | ----- | --------- | ------------ | ----- | ------ |
+|VIRB0028	|E:\Crabspy-VideoZoo	|.MP4	|FALSE	|E:\Crabspy-VideoZoo\VIRB0028.MP4	|125829120	|2017-10-13T10:30:49.000000Z	|avc1	|00:34.7	|8328320	|1040	|30000/1001	|h264	|0x31637661	|4:03	|1920	|1440
+|VIRB0016	|E:\Crabspy-VideoZoo	|.MP4	|FALSE	|E:\Crabspy-VideoZoo\VIRB0016.MP4	|2831155200	|2019-09-22T08:58:34.000000Z	|avc1	|15:00.1	|216023808	|26976	|30000/1001	|h264	|0x31637661	|4:03	|1920	|1440
+|VIRB0002	|E:\Crabspy-VideoZoo	|.MP4	|FALSE	|E:\Crabspy-VideoZoo\VIRB0002.MP4	|83886080	|2017-09-27T13:31:57.000000Z	|avc1	|00:20.7	|4968964	|1241	|60000/1001	|h264	|0x31637661	|16:09	|1920	|1080
+|VIRB0015   |E:\Crabspy-VideoZoo	|.MP4	|TRUE	| Command '['ffprobe', '-v', 'quiet', '-print_format', 'json', '-show_format', '-sexagesimal', '-show_streams', 'E:\\Crabspy-VideoZoo\\VIRB0015.MP4']' returned non-zero exit status 1| | | | | | | | | | |
+|VIRB0048   |E:\Crabspy-VideoZoo	|.MP4	|TRUE	|KeyError: 'display_aspect_ratio' | | | | | | | | | | |
+
+Please observe that this command will not populate all columns in the resulting CSV file if:
+  * files are corrupted
+  * files cannot be opened
+  * or, where ```ffprobe``` keys do not exist in multimedia streams associated to the file
---
+++ crabspy/tools/list_videos.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+"""
+Module for recursively listing all videos in a folder and extracting meta information associated to videos
+"""
+
+import os
+import csv
+from datetime import datetime
+import subprocess
+import json
+import pprint
+import argparse
+
+__author__ = "Cesar Herrera"
+__copyright__ = "Copyright (C) 2019 Cesar Herrera"
+__license__ = "GNU GPL"
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--path", default="GP010016.mov", help="Provide path to folder containing video(s)")
+args = vars(ap.parse_args())
+
+now = datetime.now()
+now = now.strftime('%Y-%m-%d %H:%M')
+file_list = open('videos.csv', 'w', newline='\n')
+
+wr = csv.writer(file_list, delimiter=',')
+wr.writerow(['File created at ' + str(now)])
+wr.writerow(['file_name', 'file_directory', 'file_extension', 'error', 'file_path', 'file_size', 'file_created',
+             'tag_major_brand', 'file_duration', 'duration_ts', 'nb_frames', 'fps', 'codec', 'codec_tag', 'aspect_ratio',
+             'width', 'height'])
+
+for root, dirs, files in os.walk(args['path']):
+    for name in files:
+        file_path = os.path.join(root, name)
+        file_path = os.path.abspath(file_path)
+        if name.endswith(('.mp4', '.MP4', '.mov', '.MOV', '.avi', '.AVI')):
+            print(name)
+            command = ['ffprobe', '-v', 'quiet', '-print_format', 'json', '-show_format', '-sexagesimal',
+                       '-show_streams', file_path]
+
+            try:
+                call_1 = subprocess.check_output(command).decode('utf-8')
+                info = json.loads(call_1)
+                pp = pprint.PrettyPrinter(indent=4)
+                pp.pprint(info)
+
+                file_name_reading = info['format']['filename']
+                format_long_name = info['format']['format_long_name']
+                size = info['format']['size']
+                created = info['format']['tags']['creation_time']
+                tag_major_brand = info['format']['tags']['major_brand']
+                duration = info['format']['duration']
+                duration_ts = info['streams'][0]['duration_ts']
+                nb_frames = info['streams'][0]['nb_frames']
+                fps = info['streams'][0]['avg_frame_rate']
+                codec = info['streams'][0]['codec_name']
+                codec_tag = info['streams'][0]['codec_tag']
+                aspect_ratio = info['streams'][0]['display_aspect_ratio']
+                width = info['streams'][0]['width']
+                height = info['streams'][0]['height']
+                error = 'FALSE'
+
+                name, name_ext = os.path.splitext(name)
+                wr.writerow([name, root, name_ext, error, file_name_reading, size, created, tag_major_brand,
+                             duration, duration_ts, nb_frames, fps, codec, codec_tag, aspect_ratio, width, height])
+                pass
+
+            except subprocess.CalledProcessError as exception:
+                name, name_ext = os.path.splitext(name)
+                error = 'True'
+                wr.writerow([name, root, name_ext, error, exception])
+
+            except KeyError as e:
+                name, name_ext = os.path.splitext(name)
+                error = 'True'
+                wr.writerow([name, root, name_ext, error, "KeyError: {}".format(e)])
+                pass
+        else:
+            # print(name)
+            pass
+file_list.close()
