ac39b8284c1da1d7cfbf429fb810d192a81acb81
Cesar Herrera <29750401+CexyNature@users.noreply.github.com>
2020-01-29 11:18:53 +1000

    Feat: Define case when duplicates come from two automatic trackers

--- crabspy/duplicates.py
+++ crabspy/duplicates.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-This scripts deals with duplicate rows in tracking file.
+This scripts deals with duplicated rows in a tracking file.
 This is normally product of tracking the same individual twice using the same or different methods.
 """
 
@@ -9,7 +9,6 @@ import os
 import argparse
 import pandas as pd
 import csv
-from datetime import datetime
 import matplotlib.pyplot as plt
 
 __author__ = "Cesar Herrera"
@@ -25,6 +24,15 @@ track = pd.read_csv("results/" + args["file"], header=2, skiprows=range(0, 1))
 track_meta = pd.read_csv("results/" + args["file"], header=None, nrows=3)
 duplicates = track[track.duplicated(["Frame_number"], keep=False)]
 
+# Find breaking points: index where
+if (duplicates["Frame_number"].diff(periods=1) != 1).any():
+    ind = duplicates.index[duplicates["Frame_number"].diff(periods=1) != 1].tolist()
+    
+    # print(ind)
+    # print(ind[1]-ind[0])
+    # print(track.iloc[ind[1]:ind[1]+65])
+else:
+    pass
 # Find overlapping frames
 # Count how many sections overlap
 # Find the tracking method used for each section
@@ -32,7 +40,7 @@ duplicates = track[track.duplicated(["Frame_number"], keep=False)]
 # Request user to select one
 # Keep selection and delete overlap
 if len(duplicates) > 0:
-    if (duplicates["tracker_method"] != "Manual_tracking").any():
+    if (duplicates["tracker_method"] == "Manual_tracking").any():
         index_2_del = duplicates.index[duplicates.tracker_method != "Manual_tracking"].tolist()
         print("Priority is given to track positions (i.e. rows) that were tracked using the method 'Manual_tracking'")
 
@@ -56,17 +64,44 @@ if len(duplicates) > 0:
         plt.gca().invert_yaxis()
         plt.show()
 
-        duplicates = duplicates[duplicates.tracker_method == "Manual_tracking"]
+        # duplicates = duplicates[duplicates.tracker_method == "Manual_tracking"]
     else:
-        print("You have duplicates, but none of these came from the method 'Manual_tracking'")
+        print("You have duplicates, but none of these came from the method 'Manual_tracking'.\n"
+              "Priority will be given to the last duplicates.")
+        tracks_obj = duplicates.tracker_method.unique()
+        print("These are the duplicated trackers {}".format(tracks_obj))
+
+        if len(tracks_obj) >= 2:
+            index_2_del = duplicates.index[duplicates.tracker_method != tracks_obj[1]].tolist()
+
+            fig = plt.figure()
+            ax1 = fig.add_subplot(1, 2, 1)
+            ax2 = fig.add_subplot(1, 2, 2, sharex=ax1, sharey=ax1)
+
+            ax1.plot(duplicates.loc[duplicates.tracker_method == tracks_obj[0], "Crab_position_x"],
+                     duplicates.loc[duplicates.tracker_method == tracks_obj[0], "Crab_position_y"])
+            ax1.title.set_text("Points from tracker {}".format(tracks_obj[0]))
+            ax1.set_xlabel('Position X')
+            ax1.set_ylabel('Position Y')
+            ax1.set_aspect("equal", "box")
+
+            ax2.plot(duplicates.loc[duplicates.tracker_method == tracks_obj[1], "Crab_position_x"],
+                     duplicates.loc[duplicates.tracker_method == tracks_obj[1], "Crab_position_y"])
+            ax2.title.set_text("Points from tracker {}".format(tracks_obj[1]))
+            ax2.set_xlabel('Position X')
+            ax2.set_aspect("equal", "box")
+
+            plt.gca().invert_yaxis()
+            plt.show()
 
     # print(duplicates)
     # print(index_2_del)
     # print(len(index_2_del))
     # print(track.shape)
     track = track.drop(index_2_del)
+    print("{} duplicated registers were deleted".format(len(index_2_del)))
     # print(track.shape)
-    print(track_meta.head())
+    # print(track_meta.head())
 
     os.makedirs("results/processed_tracks/", exist_ok=True)
 
@@ -77,7 +112,7 @@ if len(duplicates) > 0:
     #     meta_information = [next(file) for x in range(3)]
 
     meta_information = track_meta.values.tolist()
-    print(meta_information)
+    # print(meta_information)
     print("######################## ##################### ################")
 
     with open(new_file, "r") as result_in:
@@ -95,4 +130,3 @@ if len(duplicates) > 0:
 
 else:
     print("Not duplicates found")
-
